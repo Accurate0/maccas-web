7a5f85c9882d24e9df5815e4c6ec76041b6d6d81
Anurag Singh <contact@anurag.sh>
2022-07-03 19:55:27 +0800

    fix double load

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,13 @@
-import { useMsal } from "@azure/msal-react";
+import { InteractionStatus } from "@azure/msal-browser";
+import { useIsAuthenticated, useMsal } from "@azure/msal-react";
 import { useLayoutEffect } from "react";
 import { LoginRequest } from "../config/msal";
 
 const Login: React.FC = () => {
   const { instance } = useMsal();
+  const { inProgress } = useMsal();
+  const isAuthenticated = useIsAuthenticated();
+
   useLayoutEffect(() => {
     const login = async () => {
       try {
@@ -11,7 +15,9 @@ const Login: React.FC = () => {
       } catch (err) {}
     };
 
-    login();
+    if (!isAuthenticated && inProgress === InteractionStatus.None) {
+      login();
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
