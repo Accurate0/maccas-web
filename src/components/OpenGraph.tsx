9f0aaaf29d345df6eafe2c0f85109a6f7867bc26
Anurag Singh <contact@anurag.sh>
2022-08-13 14:00:24 +0800

    add more og tags

--- src/components/OpenGraph.tsx
+++ src/components/OpenGraph.tsx
@@ -4,6 +4,8 @@ const OpenGraph: React.FC = () => {
   return (
     <Head>
       <meta property="og:image" content={`${process.env.NEXT_PUBLIC_PUBLIC_URL}/og.jpg`} />
+      <meta property="og:site_name" content="Maccas" />
+      <meta property="og:description" content="what maccas should have created" />
       <meta property="og:type" content="article" />
       <meta property="twitter:card" content="summary_large_image" />
     </Head>
