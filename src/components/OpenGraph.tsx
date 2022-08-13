5e0951a57693bdc2c5922ce944789c9ccc2bb478
Anurag Singh <contact@anurag.sh>
2022-08-13 14:07:00 +0800

    og

--- src/components/OpenGraph.tsx
+++ src/components/OpenGraph.tsx
@@ -4,11 +4,23 @@ const OpenGraph: React.FC = () => {
   return (
     <Head>
       <meta charSet="utf-8" />
-      <meta property="og:site_name" content="Maccas" />
+
+      <title>Maccas</title>
+      <meta name="description" content="what maccas should have created" />
+
+      <meta property="og:url" content="https://maccas.anurag.sh" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Maccas" />
       <meta property="og:description" content="what maccas should have created" />
-      <meta property="og:image" content={`${process.env.NEXT_PUBLIC_PUBLIC_URL}/og.jpg`} />
-      <meta property="og:type" content="article" />
-      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="og:image" content="https://maccas.anurag.sh/og.jpg" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="maccas.anurag.sh" />
+      <meta property="twitter:url" content="https://maccas.anurag.sh" />
+      <meta name="twitter:title" content="Maccas" />
+      <meta name="twitter:description" content="what maccas should have created" />
+      <meta name="twitter:image" content="https://maccas.anurag.sh/og.jpg" />
+
       <meta name="theme-color" content="#DA291C" />
       <link rel="manifest" href={`${process.env.NEXT_PUBLIC_PUBLIC_URL}/manifest.json`} />
       <link rel="icon" href={`${process.env.NEXT_PUBLIC_PUBLIC_URL}/favicon.ico`} />
