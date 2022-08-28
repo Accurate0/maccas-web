81416529fe59093108b2611c5c746cdce21e7200
Anurag Singh <contact@anurag.sh>
2022-08-28 14:43:04 +0800

    dynamic width

--- src/components/DealSkeleton.tsx
+++ src/components/DealSkeleton.tsx
@@ -3,11 +3,10 @@ import { Stack, Skeleton as MuiSkeleton, Grid } from "@mui/material";
 const DealSkeleton: React.FC = () => (
   <Grid item xs={6} md={3}>
     <Stack spacing={1}>
-      <MuiSkeleton variant="rectangular" width={276} height={276} />
-      <MuiSkeleton variant="rectangular" width={276} height={210} />
-      <MuiSkeleton variant="rectangular" width={276} height={58} />
+      <MuiSkeleton variant="rectangular" height={276} />
+      <MuiSkeleton variant="rectangular" height={210} />
+      <MuiSkeleton variant="rectangular" height={58} />
     </Stack>
   </Grid>
 );
-
 export default DealSkeleton;
