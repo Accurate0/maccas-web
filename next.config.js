0a7b53db9dcf3edba507af931515714a19147cd4
Anurag Singh <contact@anurag.sh>
2022-08-28 19:45:39 +0800

    remove rewrites from conf

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  rewrites() {
-    return [
-      {
-        source: "/index.html",
-        destination: "/",
-      },
-    ];
-  },
   images: {
     domains: ["i.maccas.anurag.sh"],
   },
