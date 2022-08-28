9216e1a4fa92e4875372357b7912b3096e8aed55
Anurag Singh <contact@anurag.sh>
2022-08-28 15:56:30 +0800

    fix skeleton

--- src/components/DealSkeleton.tsx
+++ src/components/DealSkeleton.tsx
@@ -1,13 +1,19 @@
-import { Stack, Skeleton as MuiSkeleton, Grid } from "@mui/material";
+import { Stack, Skeleton as MuiSkeleton, Grid, useMediaQuery } from "@mui/material";
+import { theme } from "../theme";
 
-const DealSkeleton: React.FC = () => (
-  <Grid item xs={6} md={3}>
-    <Stack spacing={1}>
-      <MuiSkeleton variant="rectangular" height={276} />
-      <MuiSkeleton variant="rectangular" height={210} />
-      <MuiSkeleton variant="rectangular" height={58} />
-    </Stack>
-  </Grid>
-);
+const DealSkeleton: React.FC = () => {
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  const size = isMobile ? 180 : 276;
+
+  return (
+    <Grid item xs={6} md={3}>
+      <Stack spacing={1}>
+        <MuiSkeleton variant="rectangular" height={size} />
+        <MuiSkeleton variant="rectangular" height={210} />
+        <MuiSkeleton variant="rectangular" height={58} />
+      </Stack>
+    </Grid>
+  );
+};
 
 export default DealSkeleton;
