af5b73a8d836fb720c9186b8767a2e993e570942
Anurag Singh <contact@anurag.sh>
2022-12-22 20:10:30 +0800

    do nothing on login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,16 +1,4 @@
-import { useIsAuthenticated } from "@azure/msal-react";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-
 const Login: React.FC = () => {
-  const isAuthenticated = useIsAuthenticated();
-  const router = useRouter();
-  useEffect(() => {
-    if (isAuthenticated) {
-      router.push("/");
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
   return null;
 };
 
