940bdf06d4bda3ba7e5bb99295cf32750a3dfb0c
Anurag Singh <contact@anurag.sh>
2023-02-08 21:45:05 +0800

    fix og

--- src/components/OpenGraph.tsx
+++ src/components/OpenGraph.tsx
@@ -8,18 +8,18 @@ const OpenGraph: React.FC = () => {
       <title>Maccas</title>
       <meta name="description" content="what maccas should have created" />
 
-      <meta property="og:url" content="https://maccas.anurag.sh" />
+      <meta property="og:url" content="https://maccas.one" />
       <meta property="og:type" content="website" />
       <meta property="og:title" content="Maccas" />
       <meta property="og:description" content="what maccas should have created" />
-      <meta property="og:image" content="https://maccas.anurag.sh/og.jpg" />
+      <meta property="og:image" content="https://maccas.one/og.jpg" />
 
       <meta name="twitter:card" content="summary_large_image" />
-      <meta property="twitter:domain" content="maccas.anurag.sh" />
-      <meta property="twitter:url" content="https://maccas.anurag.sh" />
+      <meta property="twitter:domain" content="maccas.one" />
+      <meta property="twitter:url" content="https://maccas.one" />
       <meta name="twitter:title" content="Maccas" />
       <meta name="twitter:description" content="what maccas should have created" />
-      <meta name="twitter:image" content="https://maccas.anurag.sh/og.jpg" />
+      <meta name="twitter:image" content="https://maccas.one/og.jpg" />
 
       <meta name="theme-color" content="#DA291C" />
       <link rel="manifest" href="/manifest.json" />
