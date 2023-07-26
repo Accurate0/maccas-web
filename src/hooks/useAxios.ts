01f101877404ebc75872c629fde64aaeeabe8bf0
Anurag Singh <contact@anurag.sh>
2023-07-26 20:58:20 +0800

    client side retry

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -1,6 +1,7 @@
 import axios, { InternalAxiosRequestConfig } from "axios";
 import { useMemo } from "react";
 import useAccessToken from "./useAccessToken";
+import axiosRetry from "axios-retry";
 
 const useAxios = () => {
   const tokenPromise = useAccessToken();
@@ -9,6 +10,16 @@ const useAxios = () => {
     const axiosInstance = axios.create({
       baseURL: process.env.NEXT_PUBLIC_API_BASE,
       transformResponse: (data) => data,
+    });
+
+    axiosRetry(axiosInstance, {
+      retries: 3,
+      retryCondition: (error) => {
+        if (error.response?.status && error.response.status >= 500) {
+          return true;
+        }
+        return false;
+      },
     });
 
     axiosInstance.interceptors.request.use(
