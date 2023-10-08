e12ae84f11a5ed117ea04619fc4928e16e7e1c10
Anurag Singh <contact@anurag.sh>
2023-10-08 17:56:53 +0800

    remove

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,78 +1,8 @@
-import {
-  Box,
-  AppBar,
-  Toolbar,
-  Grid,
-  Typography,
-  Link,
-  Button,
-  useMediaQuery,
-  Drawer,
-} from "@mui/material";
-import LocationValue from "./LocationValue";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowDown91, faBars, faMoneyBill } from "@fortawesome/free-solid-svg-icons";
-import { useMemo, useState } from "react";
-import { theme } from "../theme";
-import { useLocation, useNavigate } from "react-router-dom";
-import { UserRole } from "../hooks/useApiClient/ApiClient.generated";
-import useAuthentication from "../hooks/useAuthentication";
+import { Box, AppBar, Toolbar, Grid, Typography, Link } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
   const navigate = useNavigate();
-  const [open, setOpen] = useState(false);
-  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
-  const { role } = useAuthentication();
-  const showPoints = useMemo(() => role === UserRole.Admin || role === UserRole.Privileged, [role]);
-  const location = useLocation();
-  const showLocation = location.pathname !== "/login";
-
-  const rightButtons = (direction: "row" | "column") => (
-    <Grid
-      container
-      sx={{ padding: "8px" }}
-      spacing={3}
-      direction={direction}
-      onClick={() => setOpen(false)}
-      onKeyDown={() => setOpen(false)}
-    >
-      {showPoints && (
-        <Grid item>
-          <Button sx={{ color: "white" }} onClick={() => navigate("/points")}>
-            <Grid item container spacing={1} direction="row">
-              <Grid item>
-                <FontAwesomeIcon icon={faArrowDown91} size="1x" />
-              </Grid>
-              <Grid item>
-                <Typography variant="caption">
-                  <b>Points</b>
-                </Typography>
-              </Grid>
-            </Grid>
-          </Button>
-        </Grid>
-      )}
-      <Grid item>
-        <Button sx={{ color: "white" }} onClick={() => navigate("/spending")}>
-          <Grid item container spacing={1} direction="row">
-            <Grid item>
-              <FontAwesomeIcon icon={faMoneyBill} size="1x" />
-            </Grid>
-            <Grid item>
-              <Typography variant="caption">
-                <b>Spending</b>
-              </Typography>
-            </Grid>
-          </Grid>
-        </Button>
-      </Grid>
-      {showLocation && (
-        <Grid item>
-          <LocationValue />
-        </Grid>
-      )}
-    </Grid>
-  );
 
   return (
     <Box sx={{ flexGrow: 1 }}>
