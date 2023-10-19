f64a047a3b6246d8c5031373642f009cc4475df0
Anurag Singh <contact@anurag.sh>
2023-10-19 21:29:27 +0800

    animate?

--- src/components/DealCard.tsx
+++ src/components/DealCard.tsx
@@ -1,13 +1,16 @@
-import { Grid, Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
+import { Grid, Card, CardContent, Box } from "@mui/material";
 import moment from "moment";
-import { GetDealsOffer } from "../hooks/useApiClient/ApiClient.generated";
+import { GetDealsOffer, OfferResponse } from "../hooks/useApiClient/ApiClient.generated";
 import useNotification from "../hooks/useNotification";
 import LoadableCardMedia from "./LoadableCardMedia";
 import { Typography } from "@mui/joy";
 import { truncate } from "../utils/truncate";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import DealCode from "./DealCode";
 import { useGetUserConfig } from "../hooks/useUserConfig";
+import { motion } from "framer-motion";
+import { useMutation } from "@tanstack/react-query";
+import useApiClient from "../hooks/useApiClient/useApiClient";
 
 export interface DealCardProps {
   offer: GetDealsOffer;
@@ -27,80 +30,121 @@ const DealCard: React.FC<DealCardProps> = ({ offer, onDetails: onSelect }) => {
   const validOffer = isOfferValid(offer);
   const config = useGetUserConfig();
   const notification = useNotification();
-  const [dealsSelected, setDealsSelected] = useState<string[]>([]);
+  const apiClient = useApiClient();
+  const [dealsSelected, setDealsSelected] = useState<
+    { loading: boolean; response?: OfferResponse; id: string }[]
+  >([]);
+  const onRemove = useCallback(
+    (id: string) => setDealsSelected((old) => old.filter((state) => state.id !== id)),
+    []
+  );
+
+  const addDealMutation = useMutation({
+    mutationKey: [`deal-${offer.shortName}`],
+    mutationFn: async () =>
+      (await apiClient.add_deal(offer.offerPropositionId, config!.storeId)).result,
+  });
 
   return (
     <Grid item xs={12} key={offer.dealUuid}>
-      <Accordion
-        expanded={dealsSelected.length > 0}
-        onChange={async () => {
-          if (!validOffer) {
-            notification({ msg: "This probably won't work", variant: "warning" });
-          }
-
-          if (!config?.storeId) {
-            notification({ msg: "Must select a store", variant: "error" });
-            return;
-          }
-          // use a random id so each is unique, but this isn't the deal id
-          if (dealsSelected.length >= offer.count) {
-            notification({ msg: "No more deals remain...", variant: "error" });
-          } else {
-            setDealsSelected((old) => [...old, crypto.randomUUID()]);
-          }
-        }}
-      >
-        <AccordionSummary
+      <motion.div layout>
+        <Card
           sx={{
             opacity: !validOffer ? 0.3 : undefined,
           }}
         >
-          <Grid container item direction="row" justifyContent="space-between" padding={1}>
-            <Grid item xs={8} container direction="column">
-              <Grid item xs>
-                <Typography level="h4" component="div">
-                  {truncate(offer.shortName, 20)}
-                </Typography>
+          <CardContent>
+            <Grid
+              container
+              item
+              direction="row"
+              justifyContent="space-between"
+              padding={1}
+              onClick={async () => {
+                if (!validOffer) {
+                  notification({ msg: "This probably won't work", variant: "warning" });
+                }
+
+                if (!config?.storeId) {
+                  notification({ msg: "Must select a store", variant: "error" });
+                  return;
+                }
+                // use a random id so each is unique, but this isn't the deal id
+                if (dealsSelected.length >= offer.count) {
+                  notification({ msg: "No more deals remain...", variant: "error" });
+                } else {
+                  const id = crypto.randomUUID();
+                  setDealsSelected((old) => [
+                    ...old,
+                    {
+                      id,
+                      loading: true,
+                    },
+                  ]);
+
+                  const response = await addDealMutation.mutateAsync();
+                  setDealsSelected((old) => [
+                    ...old.filter((x) => x.id !== id),
+                    {
+                      id,
+                      loading: false,
+                      response,
+                    },
+                  ]);
+                }
+              }}
+            >
+              <Grid item xs={8} container direction="column">
+                <Grid item xs>
+                  <Typography level="h4" component="div">
+                    {truncate(offer.shortName, 20)}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography level="body-sm">{offer.count} available</Typography>
+                </Grid>
+                <Grid item xs={3}>
+                  <Typography
+                    level="body-xs"
+                    textColor="black"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      onSelect();
+                    }}
+                  >
+                    <b>Details</b>
+                  </Typography>
+                </Grid>
               </Grid>
-              <Grid item>
-                <Typography level="body-sm">{offer.count} available</Typography>
-              </Grid>
-              <Grid item xs={3}>
-                <Typography
-                  level="body-xs"
-                  textColor="black"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    onSelect();
-                  }}
-                >
-                  <b>Details</b>
-                </Typography>
+              <Grid item style={{ flexBasis: "auto" }}>
+                <LoadableCardMedia
+                  image={offer.imageUrl}
+                  alt="missing image"
+                  style={{ height: 90, width: 90, display: "flex" }}
+                />
               </Grid>
             </Grid>
-            <Grid item style={{ flexBasis: "auto" }}>
-              <LoadableCardMedia
-                image={offer.imageUrl}
-                alt="missing image"
-                style={{ height: 90, width: 90, display: "flex" }}
-              />
+          </CardContent>
+          <Box paddingLeft={2} paddingRight={2}>
+            <Grid container direction="column">
+              {dealsSelected.map((state) => (
+                <Grid item paddingBottom={2}>
+                  <DealCode
+                    key={state.id}
+                    // change to deal fingerprint?
+                    // POST deals/<fingerprint> -> get random id
+                    id={offer.offerPropositionId}
+                    onRemove={() => onRemove(state.id)}
+                    loading={state.loading}
+                    code={state.response?.randomCode}
+                    dealId={state.response?.dealUuid}
+                  />
+                </Grid>
+              ))}
             </Grid>
-          </Grid>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Grid container spacing={1} direction="column">
-            {dealsSelected.map((id) => (
-              <DealCode
-                key={id}
-                // change to deal fingerprint?
-                // POST deals/<fingerprint> -> get random id
-                id={offer.offerPropositionId}
-                onRemove={() => setDealsSelected((old) => old.filter((x) => x !== id))}
-              />
-            ))}
-          </Grid>
-        </AccordionDetails>
-      </Accordion>
+          </Box>
+        </Card>
+      </motion.div>
     </Grid>
   );
 };
--- src/components/DealCode.tsx
+++ src/components/DealCode.tsx
@@ -2,86 +2,68 @@ import { Alert, Box, IconButton, LinearProgress } from "@mui/joy";
 import useApiClient from "../hooks/useApiClient/useApiClient";
 import { useGetUserConfig } from "../hooks/useUserConfig";
 import { useMutation } from "@tanstack/react-query";
-import { useEffect, useMemo } from "react";
-import { Grid } from "@mui/material";
+import { useMemo } from "react";
 import CloseIcon from "@mui/icons-material/Close";
 
 interface DealCodeProps {
   id: string;
   onRemove: () => void;
+  loading: boolean;
+  error?: string;
+  code?: string;
+  dealId?: string;
 }
 
-const DealCode = ({ id, onRemove }: DealCodeProps) => {
+const DealCode = ({ id, onRemove, loading, error, code, dealId }: DealCodeProps) => {
   const apiClient = useApiClient();
   const userConfig = useGetUserConfig();
-
-  const addDealMutation = useMutation({
-    mutationKey: [`deal-${id}`],
-    mutationFn: async () => {
-      return (await apiClient.add_deal(id, userConfig!.storeId)).result;
-    },
-  });
 
   const removeDealMutation = useMutation({
     mutationKey: [`remove-deal-${id}`],
     mutationFn: async () => {
-      if (addDealMutation.data?.dealUuid) {
-        return await apiClient.remove_deal(addDealMutation.data.dealUuid, userConfig!.storeId);
+      if (dealId) {
+        return await apiClient.remove_deal(dealId, userConfig!.storeId);
       }
     },
+    onError: onRemove,
+    onSuccess: onRemove,
   });
 
-  useEffect(() => {
-    if (addDealMutation.isIdle) {
-      addDealMutation.mutate();
-    }
-  }, [addDealMutation]);
-
   const getAddDealResponse = useMemo(() => {
-    if (addDealMutation.status === "error") {
-      return addDealMutation.error.message;
+    if (error) {
+      return error;
     }
 
-    if (addDealMutation.status === "pending" || removeDealMutation.status === "pending") {
+    if (loading || removeDealMutation.status === "pending") {
       return (
         <LinearProgress color={removeDealMutation.status === "pending" ? "danger" : "primary"} />
       );
     }
 
-    if (addDealMutation.status === "success") {
-      return (
-        <Box sx={{ fontFamily: "Monospace", fontSize: "h4.fontSize" }}>
-          {addDealMutation.data?.randomCode}
-        </Box>
-      );
+    if (!loading) {
+      return <Box sx={{ fontFamily: "Monospace", fontSize: "h4.fontSize" }}>{code}</Box>;
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [addDealMutation.status, removeDealMutation.status]);
+  }, [code, error, loading, removeDealMutation.status]);
 
   return (
-    <Grid item xs>
-      <Alert
-        sx={{ height: 24 }}
-        endDecorator={
-          <IconButton
-            variant="plain"
-            size="sm"
-            color="neutral"
-            disabled={
-              addDealMutation.status === "pending" || removeDealMutation.status === "pending"
-            }
-            onClick={async () => {
-              await removeDealMutation.mutateAsync();
-              onRemove();
-            }}
-          >
-            <CloseIcon />
-          </IconButton>
-        }
-      >
-        {getAddDealResponse}
-      </Alert>
-    </Grid>
+    <Alert
+      sx={{ height: 24 }}
+      endDecorator={
+        <IconButton
+          variant="plain"
+          size="sm"
+          color="neutral"
+          disabled={loading || removeDealMutation.status === "pending"}
+          onClick={async () => {
+            await removeDealMutation.mutateAsync();
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      }
+    >
+      {getAddDealResponse}
+    </Alert>
   );
 };
 
