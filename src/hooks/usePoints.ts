691fd9c29447060768b95e4e73d6a40ff590ab26
Anurag Singh <contact@anurag.sh>
2023-10-29 18:24:36 +0800

    disable on role

--- src/hooks/usePoints.ts
+++ src/hooks/usePoints.ts
@@ -1,14 +1,19 @@
 import useApiClient from "./useApiClient/useApiClient";
 import { useQuery } from "@tanstack/react-query";
+import useAuthentication from "./useAuthentication";
+import { UserRole } from "./useApiClient/ApiClient.generated";
 
 const usePoints = () => {
   const apiClient = useApiClient();
+  const { role } = useAuthentication();
+  const enabled = role === UserRole.Admin || role === UserRole.Privileged;
 
   return useQuery({
     queryKey: ["points"],
     queryFn: async ({ signal }) => (await apiClient.get_points(signal)).result,
     refetchOnMount: false,
     refetchOnWindowFocus: false,
+    enabled,
   });
 };
 
