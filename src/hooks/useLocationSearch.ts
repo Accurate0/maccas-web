8cc6377283ca596aaeb2aaf57f52971f60e7db6e
Anurag Singh <contact@anurag.sh>
2023-11-03 19:47:12 +0800

    use query client to search

--- src/hooks/useLocationSearch.ts
+++ src/hooks/useLocationSearch.ts
@@ -1,14 +1,19 @@
+import { useQueryClient } from "@tanstack/react-query";
 import useApiClient from "./useApiClient/useApiClient";
 import useSetBackdrop from "./useSetBackdrop";
 
 const useLocationSearch = () => {
   const setBackdrop = useSetBackdrop();
   const apiClient = useApiClient();
+  const queryClient = useQueryClient();
 
   const search = async (text: string) => {
     try {
       setBackdrop(true);
-      const response = await apiClient.search_locations(text);
+      const response = await queryClient.fetchQuery({
+        queryKey: [`search-${text}`],
+        queryFn: async ({ signal }) => await apiClient.search_locations(text, signal),
+      });
       return response.result;
     } catch (error) {
       return [];
